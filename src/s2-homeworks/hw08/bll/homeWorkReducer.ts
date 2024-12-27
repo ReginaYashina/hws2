b634ade117b3833975fe2710e95c660d91a8f76c
Regina Yashina <reginayashina@inbox.ru>
2024-12-27 21:07:23 +0300

    feat: fix HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -10,7 +10,13 @@ const initialState: UserType[] = []
 export const homeWorkReducer = (state: UserType[] = initialState, action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            return action.payload === 'up' ? state.sort((a, b) => a.name > b.name ? 1 : -1) : state.sort((a, b) => a.name < b.name ? 1 : -1)
+            if (action.payload === 'up') {
+                return state.sort((a, b) => a.name > b.name ? 1 : -1)
+            }
+            if (action.payload === 'down') {
+                return state.sort((a, b) => a.name < b.name ? 1 : -1)
+            }
+            return state
         }
         case 'check': {
             return state.filter(u => u.age >= action.payload)

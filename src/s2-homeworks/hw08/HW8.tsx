114feacde55f2b21b5b4fbb60759a75710e2aac3
Regina Yashina <reginayashina@inbox.ru>
2024-12-28 14:38:08 +0300

    feat: fix HW8-3

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -45,7 +45,6 @@ const HW8 = () => {
     const check18 = () => {
         dispatchToPeople(checkAC(18))  // совершеннолетние
         setCurrentSort('18')
-        // sortDown()
     }
 
     return (
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -10,10 +10,20 @@ const initialState: UserType[] = []
 export const homeWorkReducer = (state: UserType[] = initialState, action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            return action.payload === 'up' ? state.sort((a, b) => a.name > b.name ? 1 : -1) : state.sort((a, b) => a.name < b.name ? 1 : -1)
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) {
+                    return 1
+                } else if (a.name < b.name) {
+                    return -1
+                } else {
+                    return 0
+                }
+            })
+            return action.payload === 'up' ? newState : newState.reverse()
+
         }
         case 'check': {
-            return state.filter(u => u.age >= action.payload)
+            return [...state.filter(u => u.age >= action.payload)]
         }
         default:
             return state
